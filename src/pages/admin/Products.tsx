7b1e53a2ff5d5dc260178efd9c3eec52ff16f4f2
l0rd-AK <aka883207@gmail.com>
2025-01-08 05:03:28 +0600

    admin dashboard almost complete

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import Swal from 'sweetalert2'
 import {
   Table,
   TableBody,
@@ -11,8 +12,11 @@ import { Edit, Plus, Trash2 } from 'lucide-react';
 import AddProductModal from '../modals/AddProductModal';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import UpdateProductModal from '../modals/UpdateProductModal';
 
 export default function AdminProducts() {
+  const axios=useAxiosPublic();
   const [products, setproducts] = useState([]);
     useEffect(() => {
       fetch("http://localhost:5000/admin/products")
@@ -25,6 +29,42 @@ export default function AdminProducts() {
           toast.error('Failed to fetch products');
         });
     }, [products]);
+
+    const handleDelete = async (id) => {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const callBackFunction = async () => {
+            await axios
+              .delete(`/admin/products/${id}`)
+              .then((res) => {
+                console.log(res.data);
+                if (res.data.deletedCount > 0) {
+                  Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success",
+                  });
+                } else {
+                  Swal.fire({
+                    title: "Error!",
+                    text: "Something went wrong.",
+                    icon: "error",
+                  });
+                }
+              });
+          };
+          callBackFunction();
+        }
+      });
+    };
   return (
     <div className="container mx-auto px-4 py-8">
       <AddProductModal />
@@ -47,12 +87,8 @@ export default function AdminProducts() {
                 <TableCell>${product.price}</TableCell>
                 <TableCell>{product.stock}</TableCell>
                 <TableCell className="text-right flex gap-5">
-                  <Button variant="ghost" size="icon">
-                    <Edit className="h-4 w-4" />
-                  </Button>
-                  <Button variant="ghost" size="icon">
-                    <Trash2 className="h-4 w-4" />
-                  </Button>
+                    <UpdateProductModal Product={product} />
+                    <Trash2 onClick={()=>handleDelete(product._id)} className="h-4 w-4 text-red-600 cursor-pointer" />
                 </TableCell>
               </TableRow>
             ))}
